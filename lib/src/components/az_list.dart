1966560cb713235717f5ce60da32de29a4219a1c
Edward Vuong <edward.vuong@owllabs.com>
2023-09-04 17:53:08 -0700

    Remove header padding

--- lib/src/components/az_list.dart
+++ lib/src/components/az_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
 import 'package:sticky_az_list/src/typedef.dart';
 
@@ -26,8 +25,6 @@ class AZList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeData = Theme.of(context);
-
     return Container(
       color: options.backgroundColor,
       padding: options.padding,
@@ -41,42 +38,10 @@ class AZList extends StatelessWidget {
               .map((item) => SliverOffstage(
                     offstage: !item.children.isNotEmpty &&
                         !options.showSectionHeaderForEmptySections,
-                    sliver: SliverSafeArea(
-                      sliver: SliverStickyHeader(
-                        key: item.key,
-                        sticky: options.stickySectionHeader,
-                        header: options.showSectionHeader
-                            ? item.tag == "#" &&
-                                        options.specialSymbolBuilder != null ||
-                                    item.tag == "#" &&
-                                        defaultSpecialSymbolBuilder != null
-                                ? options.specialSymbolBuilder
-                                        ?.call(context, item.tag, null) ??
-                                    DefaultHeaderSymbol(
-                                      alignment: options.headerAligment,
-                                      symbolIcon: defaultSpecialSymbolBuilder
-                                          ?.call(context, item.tag, null),
-                                      backgroundColor: options.headerColor ??
-                                          options.backgroundColor ??
-                                          themeData.colorScheme.primary,
-                                      symbol: item.tag,
-                                    )
-                                : options.listHeaderBuilder
-                                        ?.call(context, item.tag) ??
-                                    DefaultHeaderSymbol(
-                                      alignment: options.headerAligment,
-                                      backgroundColor: options.headerColor ??
-                                          options.backgroundColor ??
-                                          themeData.colorScheme.primary,
-                                      symbol: item.tag,
-                                    )
-                            : const SizedBox.shrink(),
-                        sliver: SliverList(
-                          delegate:
-                              SliverChildListDelegate(item.children.toList()),
-                        ),
-                      ),
-                    ),
+                    sliver: SliverList(
+                      delegate:
+                      SliverChildListDelegate(item.children.toList()),
+                    )
                   ))
               .toList(),
           SliverToBoxAdapter(child: options.afterList),
